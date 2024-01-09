7a9c1886396c445fddee4b9bc4192348e2c4d0d4
krishna singhal <krishna.singhal@ivycomptech.com>
2024-01-09 20:37:29 +0530

    adding updateStock and deleteOrder api

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,22 +1,10 @@
-const Order=require("../models/orderModel")
-const errorHandler=require("../Utils/errorHandler")
-const responseHandler=require("../Utils/responseHandler")
-const Product = require("../models/productModel")
+const Order = require("../models/orderModel");
+const errorHandler = require("../Utils/errorHandler");
+const responseHandler = require("../Utils/responseHandler");
+const Product = require("../models/productModel");
 
-exports.createOrder=async(req,res)=>{
-    const
-    {
-        shippInfo,
-        orderItem,
-        paymentInfo,
-        itemPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-    
-    }=req.body
-
-const order=await Order.create({
+exports.createOrder = async (req, res) => {
+  const {
     shippInfo,
     orderItem,
     paymentInfo,
@@ -24,84 +12,98 @@ const order=await Order.create({
     taxPrice,
     shippingPrice,
     totalPrice,
-    paidAt:Date.now(),
-    user:req.user._id
-})
-res.status(200).json(
-    new responseHandler(200,"order","Order created sucessFully")
-)
-}
+  } = req.body;
 
-exports.getSingleOrder=async(req,res)=>{
-    console.log("----------->>>>>",req.params)
-const order=await Order.findById(req.params).populate("user","name email")
-if(!order){
-    throw new errorHandler(404,"Could able to find the respected order")
-}
-res.status(200).json(
-    new responseHandler(200,order,"Please check the order details")
-)
-}
+  const order = await Order.create({
+    shippInfo,
+    orderItem,
+    paymentInfo,
+    itemPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+  res
+    .status(200)
+    .json(new responseHandler(200, "order", "Order created sucessFully"));
+};
 
-exports.getMyOder=async(req,res)=>{
-    console.log("----------->>>>>",req.user.id)
-const order=await Order.findById({user:req.user.id})
-console.log("----------->>>>>>",order)
-if(!order){
-    throw new errorHandler(404,"Could able to find the respected order")
-}
-res.status(200).json(
-    new responseHandler(200,order,"Please check the order details")
-)
-}
+exports.getSingleOrder = async (req, res) => {
+  console.log("----------->>>>>", req.params);
+  const order = await Order.findById(req.params).populate("user", "name email");
+  if (!order) {
+    throw new errorHandler(404, "Could able to find the respected order");
+  }
+  res
+    .status(200)
+    .json(new responseHandler(200, order, "Please check the order details"));
+};
+
+exports.getMyOder = async (req, res) => {
+  console.log("----------->>>>>", req.user.id);
+  const order = await Order.findById({ user: req.user.id });
+  console.log("----------->>>>>>", order);
+  if (!order) {
+    throw new errorHandler(404, "Could able to find the respected order");
+  }
+  res
+    .status(200)
+    .json(new responseHandler(200, order, "Please check the order details"));
+};
 //Admin--get all product
 
-exports.getAllOrders=async(req,res)=>{
-const order=await Order.find()
+exports.getAllOrders = async (req, res) => {
+  const order = await Order.find();
 
-if(!order){
-    throw new errorHandler(404,"Could able to find the ORDERS")
-}
-let totalAmount=0
-order.forEach((order)=>{
- totalAmount+=order.totalPrice
-})
-res.status(200).json(
-    new responseHandler(200,{order,totalAmount:totalAmount},"Please check the order details")
-)
-}
+  if (!order) {
+    throw new errorHandler(404, "Could able to find the ORDERS");
+  }
+  let totalAmount = 0;
+  order.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+  res
+    .status(200)
+    .json(
+      new responseHandler(
+        200,
+        { order, totalAmount: totalAmount },
+        "Please check the order details"
+      )
+    );
+};
 // Update order by admin-----------
 
-exports.updateOrder=async(req,res)=>{
-    console.log("----------->>>>>",req.params)
-const orders=await Order.findById(req.params);
-orders.orderItem.forEach(async(item)=>{
-    await updateStock(item.product,item.quantity)
-})
-async function updateStock(productID,quantity){
-    const product=await Product.findById(productID)
-    product.stock-=quantity
-    await product.save({validateBeforeSave:false})
-}
-if(!orders){
-    throw new errorHandler(404,"Could able to find the respected order")
-}
-res.status(200).json(
-    new responseHandler(200,orders,"Please check the order details")
-)
-}
+exports.updateOrder = async (req, res) => {
+  console.log("----------->>>>>", req.params);
+  const orders = await Order.findById(req.params);
+  orders.orderItem.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+  async function updateStock(productID, quantity) {
+    const product = await Product.findById(productID);
+    product.stock -= quantity;
+    await product.save({ validateBeforeSave: false });
+  }
+  if (!orders) {
+    throw new errorHandler(404, "Could able to find the respected order");
+  }
+  res
+    .status(200)
+    .json(new responseHandler(200, orders, "Please check the order details"));
+};
 
-exports.deleteOrder=async(req,res)=>{
-    console.log("----------->>>>>",req.params)
-    
-const orders=await Order.findById(req.params);
-
-if(!orders){
-    throw new errorHandler(404,"Could able to find the respected order")
-}
-await orders.remove();
-orders.save({validateBeforeSave:fale})
-res.status(200).json(
-    new responseHandler(200,orders," order deleted sucessfully")
-)
-}
+exports.deleteOrder = async (req, res) => {
+  console.log("----------->>>>>", req.params);
+  const orders = await Order.findById(req.params);
+  if (!orders) {
+    throw new errorHandler(404, "Could able to find the respected order");
+  }
+  console.log("-------------->>>>>>>>>",orders)
+  const order = await orders.deleteOne(req.params);
+  res
+    .status(200)
+    .json(new responseHandler(200, order, " order deleted sucessfully"));
+};
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -9,5 +9,5 @@ router.route("/Order/singleOder/:_id").get(authenication,getSingleOrder)
 router.route("/Order/getMyOder").get(authenication,getMyOder)
 router.route("/Order/getAllOrders").get(getAllOrders)
 router.route("/Order/updateOrder/:_id").post(updateOrder)
-router.route("Order/deleteOrder/:_id").post(deleteOrder)
+router.route("/Order/deleteOrder/:_id").post(deleteOrder)
 module.exports=router
