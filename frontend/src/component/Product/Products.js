8e900d76898c37181892f5937af96fd7f51138f0
krishna singhal <krishna.singhal@ivycomptech.com>
2024-01-18 00:07:52 +0530

    addingProductComponent

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./Product.css"
+import { useDispatch, useSelector } from 'react-redux'
+import ProductCart from "../Home/ProductCart.js"
 
 export const Products = () => {
-  return (
-    <div>Products</div>
+    const dispatch=useDispatch();
+    const {product}=useSelector((state)=>state.product)
+    console.log("------------ProductComponents",product);
+  return (<>
+  <h2 className='productsHeading'>Prodducts</h2>
+  <div className='products'>
+        {product&&product.map((product)=><ProductCart product={product}/>)}
+    </div>
+  </>
+  
   )
 }
 
