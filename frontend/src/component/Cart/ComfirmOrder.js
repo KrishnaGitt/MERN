dd496713e6559be880b055600338a9d97cfbbf09
krishna singhal <krishna.singhal@ivycomptech.com>
2024-01-27 13:22:42 +0530

    adding js files for the compenents

---
+++ frontend/src/component/Cart/ComfirmOrder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComfirmOrder = () => {
+  return (
+    <div>ComfirmOrder</div>
+  )
+}
+
+export default ComfirmOrder
---
+++ frontend/src/component/Cart/orderSucess.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const orderSucess = () => {
+  return (
+    <div>orderSucess</div>
+  )
+}
+
+export default orderSucess
