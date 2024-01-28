93220f2b7ab034796a52917a2d9794067a3fd5e8
krishna singhal <krishna.singhal@ivycomptech.com>
2024-01-28 11:25:54 +0530

    ading cart item

--- frontend/src/component/Cart/Cart.js
+++ frontend/src/component/Cart/Cart.js
@@ -5,20 +5,28 @@ import { useSelector, useDispatch } from "react-redux";
 import { addItemsToCart } from "../../actions/cartAction";
 
 export const Cart = () => {
+    const dispatch=useDispatch();
     const item={
         name:"krishna",
         product:"apple",
         price:120,
-        quantity:10
+        quantity:10,
+        stock:10
     }
     const [subTotal,setSubTotal]=useState(0);
     const {cartItems}=useSelector((state)=>state.cart)
     console.log("--cartItems------",cartItems)
-    const [quantity,setQuantity]=useState(1);
+    // const [quantity,setQuantity]=useState(1);
     const decreaseBtn=()=>{
        
     }
     cartItems.map((crd)=>console.log("-------vairable"))
+
+    const increaseQuantity=(id,stock,quantity)=>{
+        if(stock<=quantity){return}
+        let newQauntity =quantity+1;
+            dispatch(addItemsToCart(id,newQauntity));
+    }
   return (<Fragment>
              <div className="cartPage">
                 <div className="cartHeader">
@@ -27,16 +35,32 @@ export const Cart = () => {
                     <p>subtotal</p>
                 </div>
                
-                <div className="cartContainer"> 
-                    {cartItems&&cartItems.map((crd)=><CartItemCard item={crd}/>)}
+               
+                    {cartItems&&cartItems.map((crd)=>(
+                     <div className="cartContainer"> 
+                    <CartItemCard item={crd}/>
                     <div className="cartInput">
                         <button onClick={decreaseBtn}>-</button>
-                        <input className="inputText1" type="number" value={quantity} readOnly ></input>
-                        <button>+</button>
+                        <input className="inputText1" type="number" value={item.quantity} readOnly ></input>
+                        <button onClick={()=>increaseQuantity(item.product,item.stock,item.quantity)}>+</button>
                     </div>
                 <p className="cartSubtotal">
                     {item.price*item.quantity}
                 </p>
+                </div>))}
+                <div className="cartGrossProfit">
+                    <div></div>
+                    <div className="cartGrossProfitBox">
+                        <p>{`gross totoal`}</p>
+                        <p>{`600`}</p>
+                        <p></p>
+                    </div>
+
+                    <div></div>
+                    <div className="checkOutBtn">
+                        <button>checkout</button>
+                    </div>
+
                 </div>
              </div>
            
